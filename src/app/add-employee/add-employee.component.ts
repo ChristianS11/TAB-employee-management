38ae81a991a9d0c49ec5ef2493e670c2e06d8e42
christian11 <christian.stephanus11@gmail.com>
2024-01-21 19:20:13 +0700

    first

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DatePickerComponent, DpDatePickerModule } from 'ng2-date-picker';
 
 @Component({
   selector: 'app-add-employee',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, DpDatePickerModule],
   templateUrl: './add-employee.component.html',
   styleUrl: './add-employee.component.css'
 })
 export class AddEmployeeComponent {
-
+  selectedDate: any;
+  datePickerConfig: any;
 }
